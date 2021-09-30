eb54cdcc5faa5120c080d93ed2acb3b0f85d528e
trefmanic <trefmanic@gmail.com>
2021-09-30 16:59:23 +0500

    Changed URL format

--- get-client.sh
+++ get-client.sh
@@ -17,7 +17,7 @@ wget -qO- --keep-session-cookies --save-cookies cookies1cwget --post-data "$LOGI
 # Формирование ссылки на нужную версию платформы
 # TODO: Реализовать выбор типа дистрибутива
 # TODO: Формат URL до какой-то версии отличался, сделать проверку
-PLATFORM_URL='https://releases.1c.ru/version_file?nick=Platform83&ver='$VERSION_1C'&path=Platform\'$PLATFORM_PATH'\deb64_'$PLATFORM_PATH'.tar.gz'
+PLATFORM_URL='https://releases.1c.ru/version_file?nick=Platform83&ver='$VERSION_1C'&path=Platform\'$PLATFORM_PATH'\client_'$PLATFORM_PATH'.deb64.tar.gz'
 
 # Получение прямой ссылки
 DOWNLOAD_URL=$(wget -qO- --load-cookies cookies1cwget "$PLATFORM_URL" | grep -oP '(?<=a href=")[^"]+(?=">Скачать дистрибутив<)')
@@ -27,3 +27,5 @@ wget -O deb64_$PLATFORM_PATH.tar.gz --load-cookies cookies1cwget "$DOWNLOAD_URL"
 
 # TODO: mktemp
 rm cookies1cwget
+
+exit 0
